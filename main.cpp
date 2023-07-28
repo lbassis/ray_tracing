effa1635ba5759cb2ed36224494ac8e477f46a72
Lucas Assis <assislba@gmail.com>
2023-07-28 11:38:44 -0300

    limiting number of child rays

--- main.cpp
+++ main.cpp
@@ -8,13 +8,15 @@
 
 /* posso adicionar um sleep quando acerta a esfera pra mostrar a distribuiçao de carga */
 
-color ray_color(const ray& r, const hittable& world) {
+color ray_color(const ray& r, const hittable& world, int depth) {
 
   hit_record rec;
 
+  if (depth <= 0) return color(0, 0, 0);
+  
   if (world.hit(r, 0, infinity, rec)) {
     point3 target = rec.p + rec.normal + random_in_unit_sphere();
-    return 0.5 * ray_color(ray(rec.p, target - rec.p), world);
+    return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
   }
   
   vec3 unit_direction = unit_vector(r.direction());
@@ -45,6 +47,7 @@ int main() {
   const int image_width = 400;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
   const int samples_per_pixel = 1;
+  const int max_depth = 50;
 
   // world
   hittable_list world;
@@ -68,7 +71,7 @@ int main() {
 	auto v = (j + random_double()) / (image_height-1);
 
 	ray r = cam.get_ray(u, v);
-	pixel_color += ray_color(r, world);
+	pixel_color += ray_color(r, world, max_depth);
       }      
       write_color(std::cout, pixel_color, samples_per_pixel);
     }
