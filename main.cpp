0991122a2ed8ca16e79568bdddffc58d5e9930a0
Lucas Assis <assislba@gmail.com>
2023-07-28 12:14:05 -0300

    adding uniform scatter

--- main.cpp
+++ main.cpp
@@ -21,6 +21,10 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 
     /* true lambertian method */
     point3 target = rec.p + rec.normal + random_unit_vector();
+
+    /* uniform scatter */
+    /*point3 target = rec.p + random_in_hemisphere(rec.normal);*/
+    
     return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
   }
   
