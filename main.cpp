b666b02f8d2daa8e270eaad3a8ae66a9e089aae9
Lucas Assis <assislba@gmail.com>
2023-07-28 10:06:34 -0300

    using the new functions

--- main.cpp
+++ main.cpp
@@ -1,42 +1,25 @@
 #include <iostream>
 
+#include "rtweekend.h"
 #include "color.h"
-#include "vec3.h"
-#include "ray.h"
-
+#include "hittable_list.h"
+#include "sphere.h"
 
 /* posso adicionar um sleep quando acerta a esfera pra mostrar a distribuiçao de carga */
 
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-  vec3 oc = r.origin() - center;
-
-  auto a = dot(r.direction(), r.direction());
-  auto b = 2.0 * dot(oc, r.direction());
-  auto c = dot(oc, oc) - radius*radius;
-  auto discriminant = b*b - 4*a*c; /* bhaskara */
-
-  if (discriminant < 0) {
-    return -1.0;
-  }
-  else {
-    return (- b - sqrt(discriminant)) / (2.0 * a);
-  }
-}
-
 /* sample function to create rays that create a gradient */
-color ray_color(const ray& r) {
+color ray_color(const ray& r, const hittable& world) {
 
-  auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+  hit_record rec;
 
   /* if we hit, we render the normal mapped to rgb */
-  if (t > 0.0) {
-    vec3 normal = unit_vector(r.at(t) - vec3(0, 0, -1));
-    return 0.5 * color(normal.x()+1, normal.y()+1, normal.z()+1);
+  if (world.hit(r, 0, infinity, rec)) {
+    return 0.5 * color(rec.normal + color(1, 1, 1));
   }
   
   vec3 unit_direction = unit_vector(r.direction());
-  t = 0.5*(unit_direction.y() + 1.0);
+  auto t = 0.5*(unit_direction.y() + 1.0);
 
   return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
@@ -45,9 +28,14 @@ int main() {
 
   // image
   const auto aspect_ratio = 16.0/9.0;
-  const int image_width = 400;
+  const int image_width = 1600;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
 
+  // world
+  hittable_list world;
+  world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+  world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+  
   // camera
   auto viewport_height = 2.0;
   auto viewport_width = aspect_ratio * viewport_height;
@@ -69,7 +57,7 @@ int main() {
       auto v = double(j) / (image_height-1);
 
       ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-      color pixel_color = ray_color(r);
+      color pixel_color = ray_color(r, world);
       
       write_color(std::cout, pixel_color);
     }
