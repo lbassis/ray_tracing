d2089f3e55a15162f8ff3cdc416ea73b37ed72b5
Lucas Assis <assislba@gmail.com>
2023-07-31 14:22:13 -0300

    adding timing and removing prints for the sequential algorithm

--- main.cpp
+++ main.cpp
@@ -7,36 +7,9 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include "camera.h"
+#include <chrono>
 
-#include "omp.h"
-
-struct ray_info {
-  int i;
-  int j;
-  int samples;
-  int width;
-  int height;
-  camera cam;
-  hittable_list world;
-  int max_depth;
-};
-
-color ray_color(const ray& r, const hittable& world, int depth);
-
-color process_pixel(struct ray_info info) {
-
-  color pixel_color(0, 0, 0);
-
-  for (int s = 0; s < info.samples; s++) {
-    auto u = (info.i + random_double()) / (info.width-1);
-    auto v = (info.j + random_double()) / (info.height-1);
-
-    ray r = info.cam.get_ray(u, v);
-    pixel_color += ray_color(r, info.world, info.max_depth);
-  }
-
-  return correct_color(pixel_color, info.samples);;
-}
+using namespace std::chrono;
 
 void write_image(std::vector<color> matrix, int width, int height) {
 
@@ -68,10 +41,10 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 
     /* uniform scatter */
     /*point3 target = rec.p + random_in_hemisphere(rec.normal);*/
-
+    
     return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
   }
-
+  
   vec3 unit_direction = unit_vector(r.direction());
   auto t = 0.5*(unit_direction.y() + 1.0);
 
@@ -86,7 +59,7 @@ color ray_normal_color(const ray& r, const hittable& world) {
   if (world.hit(r, 0, infinity, rec)) {
     return 0.5 * color(rec.normal + color(1, 1, 1));
   }
-
+  
   vec3 unit_direction = unit_vector(r.direction());
   auto t = 0.5*(unit_direction.y() + 1.0);
 
@@ -94,6 +67,8 @@ color ray_normal_color(const ray& r, const hittable& world) {
 }
 
 int main() {
+
+  auto start = high_resolution_clock::now();
 
   // image
   const auto aspect_ratio = 16.0/9.0;
@@ -107,20 +82,31 @@ int main() {
   hittable_list world;
   world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
   world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
-
+  
   // camera
   camera cam;
-
+  
   // render
-  omp_set_num_threads(image_width * image_height);
-#pragma omp parallel
-  {
-    int position = omp_get_thread_num();
-    ray_info info = {static_cast<int>(position%image_width), image_height - static_cast<int>(position/image_width), samples_per_pixel, image_width, image_height, cam, world, max_depth};
-    matrix[position] = process_pixel(info);
+  for (int j = image_height - 1; j >= 0; j--) {
+    for (int i = 0; i < image_width; i++) {
+
+      color pixel_color(0, 0, 0);
+      /* we will have 100 samples per pixel and use their average to calculate the color -> antialias*/
+      for (int s = 0; s < samples_per_pixel; s++) {
+	auto u = (i + random_double()) / (image_width-1);
+	auto v = (j + random_double()) / (image_height-1);
+
+	ray r = cam.get_ray(u, v);
+	pixel_color += ray_color(r, world, max_depth);
+      }      
+      matrix[(image_height-j)*image_width + i] = correct_color(pixel_color, samples_per_pixel);
+    }
   }
 
-  std::cerr << "\nDone\n";
-
   write_image(matrix, image_width, image_height);
+
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<milliseconds>(stop - start);
+ 
+  std::cout << "Duração: " << duration.count() << " milisegundos" << std::endl;
 }
